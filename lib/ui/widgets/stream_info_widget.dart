0a814a63ed07a3dffeabc450b45df92a54cbcfd6
reTsubasa <reTsubasa@gmail.com>
2021-04-30 17:58:26 +0800

    wip

---
+++ lib/ui/widgets/stream_info_widget.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SingleItem extends StatefulWidget {
+  @override
+  _SingleItemState createState() => _SingleItemState();
+}
+
+class _SingleItemState extends State<SingleItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
